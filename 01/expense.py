b7435b4716c566a2ebd7ce7cee8fad4c115f3df8
Shah Zafrani <shahzafrani@gmail.com>
2020-12-01 00:42:46 -0500

    day1 unoptimized

---
+++ 01/expense.py
@@ -0,0 +1,25 @@
+def openFile(name):
+    file1 = open(name, 'r') 
+    return file1.readlines() 
+
+def getProductOfNumsThatMakeSum(sumNum, lines):
+    notFound = True
+    while(notFound):
+        for i in range(len(lines)):
+            for j in range(len(lines)):
+                for k in range(len(lines)):
+                    a = int(lines[i])
+                    b = int(lines[j])
+                    c = int(lines[k])
+                    sum1 = a + b + c
+                    if  sum1 == sumNum:
+                        if i != j:
+                            print("{} + {} + {} = {}".format(a, b, c, sumNum))
+                            print("{} * {} * {} = {}".format(a,b,c, a*b*c))
+                            notFound=False
+
+if __name__=="__main__":
+    print("day 1: expense reports")
+    lines = openFile("input.txt")
+    print("input is {} lines long".format(len(lines)))
+    getProductOfNumsThatMakeSum(2020, lines)
