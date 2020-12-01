da6f11ca790cca27ff263dad4be3d24ab5cc1626
unknown <shahzafrani@gmail.com>
2020-12-01 01:38:46 -0500

    found the efficient way to do part 1.

--- 01/expense.py
+++ 01/expense.py
@@ -3,18 +3,30 @@ def openFile(name):
     return file1.readlines() 
 
 def getProductOfNumsThatMakeSum(sumNum, lines):
-    notFound = True
+     
+    digits = {}
+    summed_digits = {}
     for i in range(len(lines)):
-        for j in range(len(lines)):
-            for k in range(len(lines)):
-                a = int(lines[i])
-                b = int(lines[j])
-                c = int(lines[k])
-                sum1 = a + b + c
-                if  sum1 == sumNum:
-                    print("{} + {} + {} = {}".format(a, b, c, sumNum))
-                    print("{} * {} * {} = {}".format(a,b,c, a*b*c))
-                    return
+        digits[int(lines[i])] = 0
+
+    for i in range(len(lines)):
+        inti = int(lines[i])
+        diff = sumNum - inti
+        if diff in digits:
+            print("{} + {} = {}".format(int(lines[i]), diff, sumNum))
+            print("{} * {} = {}".format(int(lines[i]), diff, int(lines[i])*diff))
+    
+    # for i in range(len(lines)):
+    #     for j in range(len(lines)):
+    #         for k in range(len(lines)):
+    #             a = int(lines[i])
+    #             b = int(lines[j])
+    #             c = int(lines[k])
+    #             sum1 = a + b + c
+    #             if  sum1 == sumNum:
+    #                 print("{} + {} + {} = {}".format(a, b, c, sumNum))
+    #                 print("{} * {} * {} = {}".format(a,b,c, a*b*c))
+    #                 return
 
 if __name__=="__main__":
     print("day 1: expense reports")
