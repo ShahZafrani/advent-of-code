07a0cb37c405f0fb358502d6b110d17fc7fd54f4
z <shahzafrani@gmail.com>
2022-12-25 05:23:36 -0500

    day 4

--- 2022/04/Part2.cs
+++ 2022/04/Part2.cs
@@ -1,9 +1,9 @@
-public class Solver {
+public class Part2 {
     private string day;
     private string expectedTestOutput;
     private string testFileName;
     private string inputFileName;
-    public Solver(string day, string expectedTestOutput, string testFileName = "test.txt", string inputFileName = "input.txt") {
+    public Part2(string day, string expectedTestOutput, string testFileName = "test.txt", string inputFileName = "input.txt") {
         this.day = day;
         this.expectedTestOutput = expectedTestOutput;
         this.testFileName = testFileName;
@@ -12,7 +12,7 @@ public class Solver {
     public void Run() {
         Console.WriteLine("Solving day {0}", this.day);
         Console.WriteLine("Test Output: {0}, expected: {1}", Solve(this.testFileName), this.expectedTestOutput);
-        Console.WriteLine("\nSovled output: {0}\n", Solve(this.inputFileName));
+        Console.WriteLine("\nSovled output: {0}", Solve(this.inputFileName));
     }
     public int Solve(string fileName) {
         int fullOverlap = 0;
@@ -23,19 +23,32 @@ public class Solver {
             string[] elfOne = elfAssignments[0].Split('-');
             string[] elfTwo = elfAssignments[1].Split('-');
 
+            //2-6,4-8
             //3-76,3-3
+            //6-6,4-6
+            //5-6,2-5
+            //2-5,5-9
+            //10,11-8,9
             // Console.WriteLine(line);
             if (Int32.Parse(elfOne[0]) >= Int32.Parse(elfTwo[0])) {
-                if (Int32.Parse(elfOne[1]) <= Int32.Parse(elfTwo[1])) {
+                if (Int32.Parse(elfOne[1]) <= Int32.Parse(elfTwo[0]) || Int32.Parse(elfOne[1]) <= Int32.Parse(elfTwo[1])) {
+                    overlap = 1;
+                    // Console.WriteLine("Overlap ^^^");
+                }
+                if (Int32.Parse(elfOne[0]) <= Int32.Parse(elfTwo[1])) {
                     overlap = 1;
                     // Console.WriteLine("Overlap ^^^");
                 }
             }
             if (Int32.Parse(elfOne[0]) <= Int32.Parse(elfTwo[0])) {
-                if (Int32.Parse(elfOne[1]) >= Int32.Parse(elfTwo[1])) {
+                if (Int32.Parse(elfOne[1]) >= Int32.Parse(elfTwo[0])  || Int32.Parse(elfOne[1]) >= Int32.Parse(elfTwo[1])) {
                     overlap = 1;
                     // Console.WriteLine("Overlap ^^^");
                 }
+                // if (Int32.Parse(elfOne[0]) >= Int32.Parse(elfTwo[1])) {
+                //     overlap = 1;
+                //     // Console.WriteLine("Overlap ^^^");
+                // }
             }
             fullOverlap += overlap;
         }  
--- 2022/04/Program.cs
+++ 2022/04/Program.cs
@@ -3,5 +3,7 @@
     {
         Solver Part1 = new Solver("4", "3");
         Part1.Run();
+        Part2 part2 = new Part2("4 Part 2", "7");
+        part2.Run();
     }
 }
