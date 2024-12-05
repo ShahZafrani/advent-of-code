7e61e7f6721b6139884765b02180864ea7f4f353
z <z@zs-MacBook-Pro.local>
2024-12-05 01:08:54 -0500

    python sort custom comparators changed from 2 to 3 😰

---
+++ 2024/05/manuals.py
@@ -0,0 +1,60 @@
+import sys
+import functools
+
+def open_file(name):
+    file1 = open(name, 'r') 
+    return file1.readlines() 
+
+def parse_input(lines):
+    maps = {}
+    updates = []
+    for line in lines:
+        if "|" in line:
+            pages = line.strip().split("|")
+            if maps.get(pages[1]):
+                maps[pages[1]].append(pages[0])
+            else:
+                maps[pages[1]] = [pages[0]]
+        elif len(line) < 2:
+            pass
+        else:
+            updates.append(line.strip().split(","))
+    return maps, updates
+
+def get_valid_manual(maps, update):
+    for i, val in enumerate(update):
+        for num in update[:i]:
+            if val in maps[num]:
+                # print("{}:{}".format(val, update))
+                # print("{}:{}".format(num,maps[num]))
+                return False
+    return True
+        
+def get_repaired_manual(maps, update):
+    for i, val in enumerate(update):
+        for num in update[:i]:
+            if val in maps[num]:
+                # print("{}:{}".format(val, update))
+                # print("{}:{}".format(num,maps[num]))
+                return False
+    return True
+
+def sortt(x, y, maps):
+    if x in maps[y]:
+        return -1
+    return 1
+
+if __name__=="__main__":
+    print(sys.argv)
+    # add test after `python3 file.py` to run the test file
+    if len(sys.argv) > 1:
+        filename = "test_input.txt"
+    else:
+        filename = "input.txt"
+    print("day 5: trees or maps?")
+
+    maps, updates = parse_input(open_file(filename))
+    valid_manuals = sum([int(manual[int(len(manual)/2)]) for manual in updates if get_valid_manual(maps, manual) == True])
+    print("test output should be 143\n\toutput: {}".format(valid_manuals))
+    repaired = [sorted(manual, key=functools.cmp_to_key(lambda x, y: sortt(x, y, maps))) for manual in updates if get_valid_manual(maps, manual) == False]
+    print("part 2\n\n\ntest output should be 123\n\t{}".format(sum([int(manual[int(len(manual)/2)]) for manual in repaired])))
